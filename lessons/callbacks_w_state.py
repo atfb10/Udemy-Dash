6b66c535536c841815c6e59dabbbbf456f0ab7f3
atfb84 <atfb84@mst.edu>
2023-11-15 11:49:49 -0800

    Callback w/ state

---
+++ lessons/callbacks_w_state.py
@@ -0,0 +1,32 @@
+import dash
+from dash import (
+    dcc,
+    html
+)
+from dash.dependencies import (
+    Input,
+    Output,
+    State
+)
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+
+# read in data
+df = pd.read_csv('wheels.csv')
+
+# Create app
+app = dash.Dash()
+
+app.layout = html.Div([
+    dcc.Input(id='number-id', value=1, style={'font-size':24}),
+    html.Button(id='submit', n_clicks=0, children='Submit Here', style={'font-size':24}),
+    html.H1(id='number-out')
+])
+
+@app.callback(Output('number-out', 'children'), [Input('submit', 'n_clicks')], [State('number-id', 'value')])
+def output(n_clicks:int, number:int) -> int:
+    return f'{number} was typed in and submit btn has been clicked {n_clicks} total times'
+
+if __name__ == '__main__':
+    app.run_server()
